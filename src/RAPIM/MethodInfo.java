f0bb9ef3e6a83dc3a612d9b4d59d731ad948759e
hxh <1141495430@qq.com>
2021-12-21 10:22:08 +0800

    根据数据表修改MethodInfo类

--- src/RAPIM/MethodInfo.java
+++ src/RAPIM/MethodInfo.java
@@ -8,4 +8,72 @@ public class MethodInfo {
     public String methodDescription; //5.方法描述
     public String methodParams; //6.方法参数
     public String methodReturn; //7.方法返回信息
+
+    public MethodInfo(){}
+
+    public MethodInfo(String libraryName, String packageName, String className, String methodFullName, String methodDescription, String methodParams, String methodReturn) {
+        this.libraryName = libraryName;
+        this.packageName = packageName;
+        this.className = className;
+        this.MethodFullName = methodFullName;
+        this.methodDescription = methodDescription;
+        this.methodParams = methodParams;
+        this.methodReturn = methodReturn;
+    }
+
+    public String getLibraryName() {
+        return libraryName;
+    }
+
+    public void setLibraryName(String libraryName) {
+        this.libraryName = libraryName;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethodFullName() {
+        return MethodFullName;
+    }
+
+    public void setMethodFullName(String methodFullName) {
+        MethodFullName = methodFullName;
+    }
+
+    public String getMethodDescription() {
+        return methodDescription;
+    }
+
+    public void setMethodDescription(String methodDescription) {
+        this.methodDescription = methodDescription;
+    }
+
+    public String getMethodParams() {
+        return methodParams;
+    }
+
+    public void setMethodParams(String methodParams) {
+        this.methodParams = methodParams;
+    }
+
+    public String getMethodReturn() {
+        return methodReturn;
+    }
+
+    public void setMethodReturn(String methodReturn) {
+        this.methodReturn = methodReturn;
+    }
 }
